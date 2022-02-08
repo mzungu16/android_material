095a6f85133f6ede568d4cb143a0457cf7279b07
smirnovGleb16 <gleb.s.smirnov@gmail.com>
2022-02-08 14:43:53 +0400

    Добавил fontFamily и стиль

--- app/src/main/java/com/gleb/android_material/MainActivity.kt
+++ app/src/main/java/com/gleb/android_material/MainActivity.kt
@@ -2,11 +2,13 @@ package com.gleb.android_material
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         supportFragmentManager.beginTransaction()
             .add(R.id.container_layout, MainFragment())
             .commit()
